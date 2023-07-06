642a22d0d39f3a5b13ed3384911f813f4b1a4935
higorhms <higor_hms@outlook.com>
2023-07-06 14:47:31 -0500

    refactor: thursday class

--- 5EA/FirstSpring/FirstSpring/src/main/java/main/Main.java
+++ 5EA/FirstSpring/FirstSpring/src/main/java/main/Main.java
@@ -4,12 +4,17 @@ import entities.Game;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 //        ApplicationContext ac = new ClassPathXmlApplicationContext("config.xml");
         ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfig.class);
-
+        //environment
+        GenericXmlApplicationContext applicationContext = new GenericXmlApplicationContext();
+        applicationContext.load("config*.xml");
+        applicationContext.refresh();
+        //environment
 
         Object game = ac.getBean("game");
 
--- 5EA/FirstSpring/FirstSpring/src/main/java/main/SpringConfig.java
+++ 5EA/FirstSpring/FirstSpring/src/main/java/main/SpringConfig.java
@@ -5,8 +5,10 @@ import entities.Game;
 import entities.Vehicle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile({"development" , "default"})
 public class SpringConfig {
 
     @Bean(initMethod = "init")
